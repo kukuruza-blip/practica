c3bc7012e5c07708dc56f3a5411c4c9ab4100dd2
Vrublevskaya Polina <kukuruzavk9@gmail.com>
2026-03-23 10:12:59 +0300

    Реализован конструктор класса Firm

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PracticeWork1
+{
+    public class Firm
+    {
+        public string Name;
+        public string Address;
+        public int FoundingYear;
+
+        // Конструктор с параметрами для инициализации полей
+        public Firm(string name, string address, int year)
+        {
+            Name = name;
+            Address = address;
+            FoundingYear = year;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firm myFirm = new Firm("Техно-Горизонт", "ул. Ленина, 5", 2010);
+
+            Console.WriteLine($"Фирма: {myFirm.Name}, Год: {myFirm.FoundingYear}");
+        }
+    }
+}
